0f6868288129b2e8d0cf6eb3f01fe6bda72ccc6d
lambdastar314 <betelgeuse.star.314@gmail.com>
2020-05-09 14:40:35 +0900

    XORHashが偏るかもしれない杞憂を解消

--- src/main/kotlin/lambdastar314/simworld/util/hash/XORHash.kt
+++ src/main/kotlin/lambdastar314/simworld/util/hash/XORHash.kt
@@ -2,6 +2,10 @@ package lambdastar314.simworld.util.hash
 
 class XORHash: Hash {
     override fun hash(value: Int): Int {
+        val v = xorShift(value)
+        return v * (if(xorShift(v) % 2 == 0) 1 else -1)
+    }
+    private fun xorShift(value: Int): Int{
         var v = value
         v = v.xor(v.shl(13))
         v = v.xor(v.shr(17))
